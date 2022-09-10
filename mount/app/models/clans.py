6d7ab92442ba8abcff033fd52de7149af8880601
infernalfire72 <jkoe2502@gmail.com>
2022-09-11 00:42:24 +0200

    chore: el models

--- mount/app/models/clans.py
+++ mount/app/models/clans.py
@@ -1,6 +1,38 @@
+from datetime import datetime
 from enum import Enum
+
+from pydantic import Field
+
+from . import BaseModel
+
 
 class JoinMethod(str, Enum):
     OPEN = 'open'
     BY_REQUEST = 'by-request'
     INVITE_ONLY = 'invite-only'
+
+
+#
+# Input
+#
+class CreateClan(BaseModel):
+    name: str = Field(..., min_length=1, max_length=32)
+    tag: str = Field(..., min_length=1, max_length=8)
+    description: str
+    owner: int
+    join_method: JoinMethod
+
+class UpdateClan(BaseModel):
+    name: str | None = Field(None, min_length=1, max_length=32)
+    tag: str | None = Field(None, min_length=1, max_length=8)
+    description: str | None
+    owner: int | None
+    join_method: JoinMethod | None
+
+
+#
+# Output
+#
+class Clan(BaseModel):
+    clan_id: int
+    created_at: datetime
