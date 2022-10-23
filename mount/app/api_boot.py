594adc18e0364eed47dfdd8f8e009a410bd08bd3
cmyui <cmyuiosu@gmail.com>
2022-10-23 02:51:12 -0400

    refactor: move logging module into shared modules

--- mount/app/api_boot.py
+++ mount/app/api_boot.py
@@ -1,3 +1,8 @@
 from app.api.rest import init_api
+from app.common import settings
+from shared_modules import logger
+
+logger.configure_logging(app_env=settings.APP_ENV,
+                         log_level=settings.LOG_LEVEL)
 
 api = init_api()
